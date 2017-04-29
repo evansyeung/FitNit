1c294853cadc26356ad9db3c7591b8b59d0c4920
Evans Yeung <evansy0ung@yahoo.com>
2017-04-29 19:27:43 +0000

    Setup landing page files

---
+++ app.js
@@ -0,0 +1,25 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    User            = require("./models/user");
+
+var indexRoutes     = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/fitnit");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+//app.use(flash());
+app.set("view engine", "ejs");
+//app.use(express.static(__dirname + "/public"));
+//app.locals.moment = require('moment');
+app.use(indexRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("FitNit Server Has Started!");
+});
