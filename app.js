1d32139eeaf13cf2133fff33ba82e558dee7265f
Evans Yeung <evansy0ung@yahoo.com>
2017-05-01 04:50:35 +0000

    Add initial login files

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express         = require("express"),
     methodOverride  = require("method-override"),
     User            = require("./models/user");
 
-var indexRoutes     = require("./routes/index");
+var indexRoutes     = require("./routes/index"),
+    accountRoutes    = require("./routes/accounts");
 
 mongoose.connect("mongodb://localhost/fitnit");
 
@@ -18,7 +19,9 @@ app.use(flash());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.locals.moment = require('moment');
+
 app.use(indexRoutes);
+app.use("/account", accountRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("FitNit Server Has Started!");
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 
+// ROOT ROUTE
 router.get("/", function(req, res) {
     res.render("landing");
+});
+
+// ==================
+// LOGIN ROUTES
+// ==================
+
+// show login form
+router.get("/login", function(req, res){
+   res.render("login", {page: 'login'}); 
 });
 
 module.exports = router;
