c77311c4166b0deab2e71123101a02afabfeaa36
Evans Yeung <evansy0ung@yahoo.com>
2017-05-02 03:24:04 +0000

    Add creation date

--- routes/measurements.js
+++ routes/measurements.js
@@ -4,19 +4,26 @@ var Measurement = require("../models/measurements");
 
 // INDEX ROUTE
 router.get("/", function(req, res){
-    res.render("measurements/index");
+    Measurement.find({}, function(err, allMeasurements){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("measurements/index", {measurements: allMeasurements});
+        }
+    });
 });
 
 // CREATE ROUTE
 router.post("/", function(req, res) {
     // Get data from form and create new measurement object
+    var date = req.body.date;
     var height = req.body.height;
     var weight = req.body.weight;
     var chest = req.body.chest;
     var waist = req.body.waist;
     var hip = req.body.hip;
     
-    var newMeasurement = {height: height, weight: weight, chest: chest, waist: waist, hip: hip};
+    var newMeasurement = {createdAt: date, height: height, weight: weight, chest: chest, waist: waist, hip: hip};
     
     // Create new measurement to add to DB
     Measurement.create(newMeasurement, function(err, measurement) {
