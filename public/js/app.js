972296f64841b29e354b9abdbdec69014943f894
Evans Yeung <evansy0ung@yahoo.com>
2017-05-06 05:13:34 +0000

    Update show page for programs

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,6 @@ $(".fa-plus-circle").on("click", function() {
     if(inputCount === 1) {
         $(".fa-minus-circle").fadeToggle();
     }
-//   $("#appendholder").append(
-//       '<div class="form-group"><label for="exercises">Exercise</label><input class="form-control" type="text" name="exercises" placeholder="name" maxlength="30" required></div><div class="form-group"><div class="set-input"><label for="sets">Sets</label><input class="form-control" type="number" name="sets" placeholder="sets" min="1" step="1" required></div><div class="rep-input"><label for="reps">Reps</label><input class="form-control" type="text" name="reps" placeholder="reps" required></div></div>'
-//       );
     $('<div><div class="form-group"><label for="exercises">Exercise</label><input class="form-control" type="text" name="exercises" placeholder="name" maxlength="30" required></div><div class="form-group"><div class="set-input"><label for="sets">Sets</label><input class="form-control" type="number" name="sets" placeholder="sets" min="1" step="1" required></div><div class="rep-input"><label for="reps">Reps</label><input class="form-control" type="text" name="reps" placeholder="reps" required></div></div></div>').insertAfter("form div:nth-last-of-type(3)");
     inputCount++;
 });
