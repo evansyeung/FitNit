f5f65f0bcc10f88260beb879ac3b36f012a117a1
Evans Yeung <evansy0ung@yahoo.com>
2017-05-08 06:27:09 +0000

    Add plus and minus button animation

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,8 @@ $(".fa-plus-circle").on("click", function() {
     if(inputCount === 1) {
         $(".fa-minus-circle").fadeToggle();
     }
+    $(this).animate({fontSize: '70px'});
+    $(this).animate({fontSize: '40px'});
     // $('<div><div class="form-group"><label for="exercises">Exercise</label><input class="form-control" type="text" name="exercises" placeholder="name" maxlength="30" required></div><div class="form-group"><div class="set-input"><label for="sets">Sets</label><input class="form-control" type="number" name="sets" placeholder="sets" min="1" step="1" required></div><div class="rep-input"><label for="reps">Reps</label><input class="form-control" type="text" name="reps" placeholder="reps" required></div></div></div>').insertAfter("form div:nth-last-of-type(3)");
     $('<div class="form-group"><label for="exercises">Exercise ' + (inputCount + 1) + '</label><input class="form-control" type="text" name="exercises" placeholder="name" maxlength="30" required></div><div class="form-group"><label for="sets">Sets</label><input class="form-control" type="number" name="sets" placeholder="sets" min="1" step="1" required></div><div class="form-group"><label for="reps">Reps</label><input class="form-control" type="text" name="reps" placeholder="reps" required></div>').insertAfter("form div:nth-last-of-type(3)");
     inputCount++;
@@ -12,6 +14,8 @@ $(".fa-plus-circle").on("click", function() {
 
 $(".fa-minus-circle").on("click", function() {
     if(inputCount > 1) {
+        $(this).animate({fontSize: '10px'});
+        $(this).animate({fontSize: '40px'});
         $("form div:nth-last-of-type(3)").remove();
         $("form div:nth-last-of-type(3)").remove();
         $("form div:nth-last-of-type(3)").remove();
