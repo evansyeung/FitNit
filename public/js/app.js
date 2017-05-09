9df5127019e3831d4271e24f7d6de759cace216f
Evans Yeung <evansy0ung@yahoo.com>
2017-05-09 03:30:32 +0000

    Add css to buttons

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ $(".fa-plus-circle").on("click", function() {
         $(".fa-minus-circle").fadeToggle();
     }
     $(this).animate({fontSize: '70px'});
-    $(this).animate({fontSize: '40px'});
+    $(this).animate({fontSize: '30px'});
     $('<div class="form-group"><label for="exercises">Exercise ' + (inputCount + 1) + '</label><input class="form-control" type="text" name="exercises" placeholder="name" maxlength="30" required></div><div class="form-group"><label for="sets">Sets</label><input class="form-control" type="number" name="sets" placeholder="sets" min="1" step="1" required></div><div class="form-group"><label for="reps">Reps</label><input class="form-control" type="text" name="reps" placeholder="reps" required></div>').insertAfter("form div:nth-last-of-type(3)");
     inputCount++;
 });
@@ -14,7 +14,7 @@ $(".fa-plus-circle").on("click", function() {
 $(".fa-minus-circle").on("click", function() {
     if(inputCount > 1) {
         $(this).animate({fontSize: '10px'});
-        $(this).animate({fontSize: '40px'});
+        $(this).animate({fontSize: '30px'});
         $("form div:nth-last-of-type(3)").remove();
         $("form div:nth-last-of-type(3)").remove();
         $("form div:nth-last-of-type(3)").remove();
